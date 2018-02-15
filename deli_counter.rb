ca7116ccadb1ad4a1d91aee14b125f4ec31a561f
emi kojima <emi8emi@gmail.com>
2018-02-15 18:39:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,8 +22,7 @@ end
 
 def now_serving(katz_deli)
     if katz_deli.count > 0
-      katz_deli.shift
-    puts "Currently serving #{katz_deli.shift}."
+    puts "Currently serving #{katz_deli[0]}."
     
    else
     puts "There is nobody waiting to be served!"
