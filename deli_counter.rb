e0d866254b3a315892468e0b442cfcaf596b96d9
emi kojima <emi8emi@gmail.com>
2018-02-15 18:25:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 
 def now_serving(katz_deli)
   until katz_deli.count == 0
-   puts "Currently serving #{katz_deli[0]}."
+    puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
     end 
     puts "There is nobody waiting to be served!"
